f54f765d248431f35247b0973ce80c4396e7e0c1
PRATYUSH18SONI <76219367+PRATYUSH18SONI@users.noreply.github.com>
2022-10-11 23:39:22 +0530

    Create binarysearch.cpp

---
+++ binarysearch.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int array[], int x, int low, int high) {
+  
+	// Repeat until the pointers low and high meet each other
+  while (low <= high) {
+    int mid = low + (high - low) / 2;
+
+    if (array[mid] == x)
+      return mid;
+
+    if (array[mid] < x)
+      low = mid + 1;
+
+    else
+      high = mid - 1;
+  }
+
+  return -1;
+}
+
+int main(void) {
+  int array[] = {3, 4, 5, 6, 7, 8, 9};
+  int x = 4;
+  int n = sizeof(array) / sizeof(array[0]);
+  int result = binarySearch(array, x, 0, n - 1);
+  if (result == -1)
+    printf("Not found");
+  else
+    printf("Element is found at index %d", result);
+}
