c5f554cbf26e1f29a3d01242ac48202edb97a852
SOMAN-SABEEL <84325440+SOMAN-SABEEL@users.noreply.github.com>
2023-10-02 08:55:19 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,13 @@
+def add_numbers(a, b):
+    """
+    This function adds two numbers and returns the result.
+    
+    :param a: The first number
+    :param b: The second number
+    :return: The sum of a and b
+    """
+    return a + b
+
+# Example usage
+result = add_numbers(5, 3)
+print("The sum is:", result)
