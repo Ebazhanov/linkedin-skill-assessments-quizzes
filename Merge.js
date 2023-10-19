03c31d6f1f0045f13b9f3fe5d20cee5cc1f9fb06
Mahul22 <121723582+Mahul22@users.noreply.github.com>
2023-10-19 10:45:12 +0530

    Create Merge.js

---
+++ Merge.js
@@ -0,0 +1,93 @@
+// JavaScript program for Merge Sort
+
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+function merge(arr, l, m, r)
+{
+	var n1 = m - l + 1;
+	var n2 = r - m;
+
+	// Create temp arrays
+	var L = new Array(n1); 
+	var R = new Array(n2);
+
+	// Copy data to temp arrays L[] and R[]
+	for (var i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (var j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+
+	// Merge the temp arrays back into arr[l..r]
+
+	// Initial index of first subarray
+	var i = 0;
+
+	// Initial index of second subarray
+	var j = 0;
+
+	// Initial index of merged subarray
+	var k = l;
+
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	// Copy the remaining elements of
+	// L[], if there are any
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	// Copy the remaining elements of
+	// R[], if there are any
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+// l is for left index and r is
+// right index of the sub-array
+// of arr to be sorted
+function mergeSort(arr,l, r){
+	if(l>=r){
+		return;
+	}
+	var m =l+ parseInt((r-l)/2);
+	mergeSort(arr,l,m);
+	mergeSort(arr,m+1,r);
+	merge(arr,l,m,r);
+}
+
+// Function to print an array
+function printArray( A, size)
+{
+	for (var i = 0; i < size; i++)
+	console.log( A[i] + " ");
+}
+
+
+var arr = [ 12, 11, 13, 5, 6, 7 ];
+	var arr_size = arr.length;
+
+	console.log( "Given array is ");
+	printArray(arr, arr_size);
+
+	mergeSort(arr, 0, arr_size - 1);
+
+	console.log( "Sorted array is ");
+	printArray(arr, arr_size);
+
+// This code is contributed by SoumikMondal
